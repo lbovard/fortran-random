99ac5e7d5f5c3333b46a30cfd8fd776807e81a75
Luke Bovard <lbovard@high-fructose-corn-syrup.csclub.uwaterloo.ca>
2013-01-21 12:57:22 -0500

    Some tests of forall loops w/ links to discussion on SO

--- forall_test.f90
+++ forall_test.f90
@@ -1,10 +1,13 @@
+! some discussion
+! http://stackoverflow.com/questions/8602596/fortran-forall-restrictions
+! this test seems to indicate that forall is just as fast as nested do loops
 program foralltest
   implicit none
   real(kind=8),  dimension(:,:),allocatable :: A  
   real(kind=8) :: t1,t2
   integer :: i,j,N
 
-  N=1000
+  N=10000
   allocate(A(N,N))
   
   call cpu_time(t1)
@@ -12,6 +15,13 @@ program foralltest
   call cpu_time(t2)
   print *, 'Elapsed time = ', t2-t1
 
+  A=0
+  call cpu_time(t1)
+  forall(j=1:N,i=1:N) A(j,i)=i+j
+  call cpu_time(t2)
+  print *, 'Elapsed time = ', t2-t1
+
+  A=0
   call cpu_time(t1) 
   do i=1,N
     do j=1,N
